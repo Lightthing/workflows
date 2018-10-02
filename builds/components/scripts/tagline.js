f85bad0a97a702c6dbc58f39ca48480a5cb20da5
Tymofii <lightthing@i.ua>
2018-10-02 06:00:01 +0300

    Added CoffeScript files

---
+++ builds/components/scripts/tagline.js
@@ -0,0 +1,7 @@
+var fill;
+
+(fill = function(item) {
+  return $('#tagline').append(`${item}`);
+})('The most beautiful city in the world');
+
+fill;
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
 
-gulp.task('log', function(){
-	gutil.log('Timka hello!');
+var coffeeSources = ['builds/components/coffee/tagline.coffee'];
+
+gulp.task('coffee', function(){
+	gulp.src(coffeeSources).pipe(coffee({bare: true}).on('error', gutil.log)).pipe(gulp.dest("builds/components/scripts"));
 });
