6a0211680ea703f1b733073183ff181380de6040
Tymofii <lightthing@i.ua>
2018-10-03 08:34:01 +0300

    Added switching between development and production modes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 // Link gulp plugins (may be installed rirst through node.js npm)
 
+
 var gulp = require('gulp'); //core module for Gulp
 var gutil = require('gulp-util'); // Gulp utilities library, e.g. 'log' method
 var coffee = require('gulp-coffee'); // Processes CoffeeScript into JavaScript
@@ -8,44 +9,76 @@ var browserify = require('gulp-browserify'); // Mixes libraries (like JQuery) in
 var compass = require('gulp-compass'); // Processes Sass into CSS (needs Ruby to be installed)
 var connect = require('gulp-connect'); // Creates HTTP server
 
+
+// Variables declaration
+
+var htmlSources, coffeeSources, jsSources, sassSources, outputDir, sassStyle;
+
+
+
+// ENVIRONMENT
+//Switching between development and production modes
+var production_mode = 0;
+
+if(!production_mode){//Running Gulp in development mode
+	gutil.log('<<<<<<< DEVELOPMENT MODE >>>>>>>');
+	outputDir = 'builds/development/';
+	sassStyle = 'expanded';
+}else{//Running Gulp in production mode
+	gutil.log('<<<<<<< PRODUCTION MODE >>>>>>>');
+	outputDir = 'builds/production/';
+	sassStyle = 'compressed';
+}
+
+
+
 // Sources
 
-var coffeeSources = [
+htmlSources = [
+	outputDir + 'index.html'
+];
+
+coffeeSources = [
 	'builds/components/coffee/tagline.coffee'
 ];
 
-var jsSources = [
+jsSources = [
 	'builds/components/scripts/tagline.js',
 	'builds/components/scripts/h1_enlarger.js',
 	'builds/components/scripts/img_enlarger.js'
 ];
 
-var sassSources = [
+sassSources = [
 	'builds/components/sass/style.scss'
 ];
 
 // Gulp tasks
+
+gulp.task('html_track', function(){
+	gulp.src(htmlSources).pipe(connect.reload());
+});
 
 gulp.task('coffee', function(){
 	gulp.src(coffeeSources).pipe(coffee({bare: true}).on('error', gutil.log)).pipe(gulp.dest("builds/components/scripts"));
 });
 
 gulp.task('js_concat', function(){
-	gulp.src(jsSources).pipe(concat('script.js')).on('error', gutil.log).pipe(browserify()).pipe(gulp.dest('builds/development/js')).pipe(connect.reload());
+	gulp.src(jsSources).pipe(concat('script.js')).on('error', gutil.log).pipe(browserify()).pipe(gulp.dest(outputDir + 'js')).pipe(connect.reload());
 });
 
 gulp.task('compass', function(){
 	gulp.src(sassSources).pipe(compass({
 		sass: 'builds/components/sass',
-		image: 'builds/development/images',
-		style: 'expanded'
-	})).on('error', gutil.log).pipe(gulp.dest('builds/development/css')).pipe(connect.reload());
+		image: outputDir + 'images',
+		style: sassStyle
+	})).on('error', gutil.log).pipe(gulp.dest(outputDir + 'css')).pipe(connect.reload());
 });
 
 gulp.task('all', ['coffee', 'js_concat', 'compass']);
 
 
 gulp.task('watch', function(){
+	gulp.watch(htmlSources, ['html_track']);
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js_concat']);
 	gulp.watch('builds/components/sass/*.scss', ['compass']);
@@ -53,9 +86,9 @@ gulp.task('watch', function(){
 
 gulp.task('connect', function(){
 	connect.server({
-		root: 'builds/development',
+		root: outputDir,
 		livereload: true
 	});
 });
 
-gulp.task('default', ['coffee', 'js_concat', 'compass', 'connect', 'watch']);
+gulp.task('default', ['html_track', 'coffee', 'js_concat', 'compass', 'connect', 'watch']);
